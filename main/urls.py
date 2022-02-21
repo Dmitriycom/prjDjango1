6d8d95ccba1602a77977e05726079e58709a82dd
Dmitriy <dima.chumak0603@gmail.com>
2022-02-21 17:40:58 +0200

    Creating a function to display the timetable of the day

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,9 @@
 from django import views
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path
 from . import views
 
 
 urlpatterns = [
-    path('', views.index, name="index"),
+    path("<str:day>", views.index, name="index"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from soupsieve import match
 
-def index(request):
-    return HttpResponse("Hello World!")
+
+ttable = [['Інформатика', 'Фізкультура', 'Література', 'Українська мова'],
+['Інформатика', 'Математика', 'Література', 'Географія'],
+['Фізкультура', 'Математика', 'Математика', 'Українська мова'],
+['Математика', 'Інформатика', 'Література', 'Біологія'],
+['Література', 'Математика', 'Інформатика', 'Фізкультура']]
+
+
+def index(request, day):
+    response = None
+    if day in {'monday', '1', 'Monday'}:
+        response = '<h1> Понеділок </h1> <br>'
+        for a in ttable[0]: response = response + f'{ttable[0].index(a)+1}. ' + a + '<br>'
+    elif day in {'tuesday', '2', 'Tuesday'}:
+        response = '<h1> Вівторок </h1> <br>'
+        for a in ttable[1]: response = response + f'{ttable[1].index(a)+1}. ' + a + '<br>'
+    elif day in {'wednesday', '3', 'Wednesday'}:
+        response = '<h1> Середа </h1> <br>'
+        for a in ttable[2]: response = response + f'{ttable[2].index(a)+1}. ' + a + '<br>'
+    elif day in {'thursday', '4', 'Thursday'}:
+        response = '<h1> Четвер </h1> <br>'
+        for a in ttable[3]: response = response + f'{ttable[3].index(a)+1}. ' + a + '<br>'
+    elif day in {'friday', '5', 'Friday'}:
+        response = '<h1> П\'ятниця </h1> <br>'
+        for a in ttable[4]: response = response + f'{ttable[4].index(a)+1}. ' + a + '<br>'
+    else:
+       response = '<h1> Неділля </h1> <br> Вихідий'
+    return HttpResponse(response)
